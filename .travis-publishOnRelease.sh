4ebedb16607aca22f7787b230381ec148b7b7d04
Franz Becker <becker.franz@gmail.com>
2016-03-01 15:10:10 +0100

    TE-322 improve release script

--- .travis-publishOnRelease.sh
+++ .travis-publishOnRelease.sh
@@ -4,8 +4,8 @@
 # Execute only on tag builds where the tag starts with 'v'
 if [[ -n "$TRAVIS_TAG" && "$TRAVIS_TAG" == v* ]]; then
     echo "Publishing version: $TRAVIS_TAG"
+    echo "" >> gradle.properties
     echo "gradle.publish.key=$GRADLE_PUBLISH_KEY" >> gradle.properties
     echo "gradle.publish.secret=$GRADLE_PUBLISH_SECRET" >> gradle.properties
-    ./gradlew bintrayUpload
     ./gradlew publishPlugins
 fi

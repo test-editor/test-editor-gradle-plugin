c2f5fc97eb3d11ce7ede1eeb10b04da0af0fb205
Franz Becker <becker.franz@gmail.com>
2016-03-01 11:44:35 +0100

    TE-322 prepare first release

---
+++ .travis-publishOnRelease.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Taken from https://github.com/franzbecker/gradle-lombok/blob/master/.travis-publishOnRelease.sh
+
+# Execute only on tag builds where the tag starts with 'v'
+if [[ -n "$TRAVIS_TAG" && "$TRAVIS_TAG" == v* ]]; then
+    echo "Publishing version: $TRAVIS_TAG"
+    echo "gradle.publish.key=$GRADLE_PUBLISH_KEY" >> gradle.properties
+    echo "gradle.publish.secret=$GRADLE_PUBLISH_SECRET" >> gradle.properties
+    ./gradlew bintrayUpload
+    ./gradlew publishPlugins
+fi

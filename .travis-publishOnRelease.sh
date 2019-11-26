d48d333867849726fec2793346dce4a731e0ae18
kbayraktar <kbayraktar@web.de>
2019-11-26 15:43:53 +0100

    Bintray Upload ist restored Because the Gradle Plugin Sites does not react for our support requests. Now the Gradle Plugin will be stored on the Bintray Sites https://bintray.com/test-editor/maven

--- .travis-publishOnRelease.sh
+++ .travis-publishOnRelease.sh
@@ -3,9 +3,5 @@
 
 # Execute only on tag builds where the tag starts with 'v'
 if [[ -n "$TRAVIS_TAG" && "$TRAVIS_TAG" == v* ]]; then
-    echo "Publishing version: $TRAVIS_TAG"
-    echo "" >> gradle.properties
-    echo "gradle.publish.key=$GRADLE_PUBLISH_KEY" >> gradle.properties
-    echo "gradle.publish.secret=$GRADLE_PUBLISH_SECRET" >> gradle.properties
-    ./gradlew publishPlugins
+    ./gradlew bintrayUpload
 fi

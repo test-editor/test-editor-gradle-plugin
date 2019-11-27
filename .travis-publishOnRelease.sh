329eadbcc7bac11d78ab7bf3315d61eaa1d140da
kbayraktar <kbayraktar@web.de>
2019-11-27 11:56:03 +0100

    Output for starting Bintray upload added.

--- .travis-publishOnRelease.sh
+++ .travis-publishOnRelease.sh
@@ -3,5 +3,6 @@
 
 # Execute only on tag builds where the tag starts with 'v'
 if [[ -n "$TRAVIS_TAG" && "$TRAVIS_TAG" == v* ]]; then
+    echo "Starting publishing version: $TRAVIS_TAG"
     ./gradlew bintrayUpload
 fi

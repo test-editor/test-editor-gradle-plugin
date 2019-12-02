1aa3c66862c235faece7e961e49085ce54095d46
kbayraktar <kbayraktar@users.noreply.github.com>
2019-12-02 14:17:21 +0100

    Bintray Upload ist restored (#21)
    * Bintray Upload ist restored
    Because the Gradle Plugin Sites does not react for our support requests.
    Now the Gradle Plugin will be stored on the Bintray Sites
    https://bintray.com/test-editor/maven
    
    * Output for starting Bintray upload added.
    
    * Version from 0.18 decremented to 0.14
    because the steps between were only unsuccessful deployments to the
    Gradle Plugin Center.
    
    * Version of released gradle-plugin changed to 0.14

--- .travis-publishOnRelease.sh
+++ .travis-publishOnRelease.sh
@@ -3,9 +3,6 @@
 
 # Execute only on tag builds where the tag starts with 'v'
 if [[ -n "$TRAVIS_TAG" && "$TRAVIS_TAG" == v* ]]; then
-    echo "Publishing version: $TRAVIS_TAG"
-    echo "" >> gradle.properties
-    echo "gradle.publish.key=$GRADLE_PUBLISH_KEY" >> gradle.properties
-    echo "gradle.publish.secret=$GRADLE_PUBLISH_SECRET" >> gradle.properties
-    ./gradlew publishPlugins
+    echo "Starting publishing version: $TRAVIS_TAG"
+    ./gradlew bintrayUpload
 fi
